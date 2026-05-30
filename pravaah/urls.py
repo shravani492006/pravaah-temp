451574a417fc4be66afee1785cf8f863a3432af6
shravani492006 <fs22if005@gmail.com>
2026-05-30 06:17:39 +0530

    changes in ui

--- pravaah/urls.py
+++ pravaah/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     # Admin
     path('admin/', admin.site.urls),
 
     # Root -> Login (make login the first page)
-    from django.views.generic import RedirectView
+    
     path('', RedirectView.as_view(url='/accounts/login/', permanent=False)),
 
     # Dashboard (moved)
