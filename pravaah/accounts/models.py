144bee950ac5372bf2448a1074428d9d229fbcd0
shravani492006 <fs22if005@gmail.com>
2026-05-30 06:38:08 +0530

    changes in models

--- pravaah/accounts/models.py
+++ pravaah/accounts/models.py
@@ -7,6 +7,7 @@ from django.dispatch import receiver
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     must_change_password = models.BooleanField(default=False)
+    sidebar_collapsed = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Profile: {self.user.username}"
